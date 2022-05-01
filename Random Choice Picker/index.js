80006a6fbb16b252bc86074499370b43ebbcbad1
Muneeb <muneebmughalr@gmail.com>
2022-05-02 03:28:11 +0500

    Random Choice Selector

---
+++ Random Choice Picker/index.js
@@ -0,0 +1,39 @@
+const textArea = document.getElementById("textarea");
+const tagsEl = document.querySelector(".tags");
+textArea.addEventListener("keyup", (e) => {
+  makeTags(e.target.value);
+  if (e.key === "Enter") {
+    e.target.value = "";
+    selectRandomTag();
+  }
+});
+textArea.focus();
+function makeTags(value) {
+  const tagValues = value
+    .split(",")
+    .filter((val) => val.trim() !== "")
+    .map((tag) => tag.trim());
+  tagsEl.innerHTML = "";
+  tagValues.forEach((tagVal) => {
+    const tag = document.createElement("span");
+    tag.classList.add("tag");
+    tag.innerText = tagVal;
+    tagsEl.appendChild(tag);
+  });
+}
+function selectRandomTag() {
+  const interval = setInterval(() => {
+    const randomTag =
+      tagsEl.childNodes[Math.floor(Math.random() * tagsEl.childNodes.length)];
+    randomTag.classList.add("selected");
+    setTimeout(() => {
+      randomTag.classList.remove("selected");
+    }, 100);
+  }, 100);
+  setTimeout(() => {
+    clearInterval(interval);
+    const randomTag =
+      tagsEl.childNodes[Math.floor(Math.random() * tagsEl.childNodes.length)];
+    randomTag.classList.add("selected");
+  }, 3000);
+}
